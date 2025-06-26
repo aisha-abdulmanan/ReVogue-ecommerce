112cb254ab446f168cc9deb1487f11daa4d6b270
aisha-abdulmanan <abdulmanan.sc123@s.msumain.edu.ph>
2025-06-27 07:54:13 +0800

    Minor changes on home page

--- FRONTEND/pages/index.vue
+++ FRONTEND/pages/index.vue
@@ -3,14 +3,16 @@
   <!-- Home Page -->
   <div class="min-h-screen flex items-center justify-center">
     <div class="text-center max-w-2xl">
+
       <!-- Brand Name -->
       <p class="text-7xl font-extrabold text-purple-900 mb-6"> 🛍️ ReVogue </p>
 
-      <!-- Smaller paragraph description -->
+      <!-- Tagline -->
       <p class="text-2xl italic text-gray-700 mb-5">
         Revive old clothes with style and sustainability in mind 🌿
       </p>
     
+      <!-- Button to Product Page -->
       <NuxtLink
         to="/product"
         class="inline-block bg-purple-700 text-white text-lg font-medium px-6 py-3 rounded-full shadow-md hover:bg-purple-800 transition"
