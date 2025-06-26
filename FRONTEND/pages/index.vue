51e8f00dbe99d192411e81b80734240b1854dd2a
aisha-abdulmanan <abdulmanan.sc123@s.msumain.edu.ph>
2025-06-27 07:27:03 +0800

    Add a button to product page on home page

--- FRONTEND/pages/index.vue
+++ FRONTEND/pages/index.vue
@@ -2,14 +2,21 @@
 
   <!-- Home Page -->
   <div class="min-h-screen flex items-center justify-center">
-    <div class="text-center">
-      <!-- Big and bold heading -->
+    <div class="text-center max-w-2xl">
+      <!-- Brand Name -->
       <p class="text-7xl font-extrabold text-purple-900 mb-6"> 🛍️ ReVogue </p>
 
       <!-- Smaller paragraph description -->
-      <p class="text-2xl italic text-gray-700">
+      <p class="text-2xl italic text-gray-700 mb-5">
         Revive old clothes with style and sustainability in mind 🌿
       </p>
+    
+      <NuxtLink
+        to="/product"
+        class="inline-block bg-purple-700 text-white text-lg font-medium px-6 py-3 rounded-full shadow-md hover:bg-purple-800 transition"
+      >
+        Shop Now
+      </NuxtLink>
 
     </div>
   </div>
