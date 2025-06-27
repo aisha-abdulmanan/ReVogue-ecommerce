f764d3a100aebe506b681eb95e1f78b082ab5ff9
aisha-abdulmanan <abdulmanan.sc123@s.msumain.edu.ph>
2025-06-27 17:13:15 +0800

    Add dummy data to user and cart-item seeder

--- BACKEND/seeders/20250627090723-demo-cart-item.js
+++ BACKEND/seeders/20250627090723-demo-cart-item.js
@@ -2,24 +2,35 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('CartItems', [
+      {
+        name: 'Boho Skirt',
+        price: 389.75,
+        quantity: 1,
+        image: '/images/skirt-8.jpg',
+        store: 'Preloved Finds',
+        selected: true,
+        userId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Corduroy Pants',
+        price: 499.5,
+        quantity: 2,
+        image: '/images/pants.jpg',
+        store: 'Thrift Store A',
+        selected: true,
+        userId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('CartItems', null, {});
+  },
 };
+
